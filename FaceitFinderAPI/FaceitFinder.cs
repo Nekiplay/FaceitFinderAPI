bb276701fcf176920ad43700d717beb0b09d45ac
Neki play <Herobrine858@outlook.com>
2021-05-08 02:49:15 +0500

    Добавьте файлы проекта.

---
+++ FaceitFinderAPI/FaceitFinder.cs
@@ -0,0 +1,42 @@
+﻿using Leaf.xNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FaceitFinderAPI
+{
+    public class FaceitFinder
+    {
+        public Responce GetResponce(string steam_id)
+        {
+            using (var request = new HttpRequest())
+            {
+                request.UserAgent = Http.ChromeUserAgent();
+
+                // Отправляем запрос.
+                HttpResponse response = request.Get("https://faceitfinder.com/profile/" + steam_id);
+                // Принимаем тело сообщения в виде строки.
+                string site = response.ToString();
+
+                Console.WriteLine(site);
+                Responce responce = new Responce();
+                try
+                {
+                    responce.Faceit = new Uri(Regex.Match(site, "<a target=\"_blank\" class=\"nolink\" href=\"(.*)\" rel=\"noopener\"><img class=\"account-faceit-flag\"").Groups[1].Value);
+                } catch { }
+                responce.Trust = double.Parse(Regex.Match(site, "<div class=\"steam-trust-progress-red\" style=\"width:(.*)%\"></div><div class=\"steam-trust-defaultprogress\"").Groups[1].Value.Replace(".", ","));
+                return responce;
+            }
+        }
+        public class Responce
+        {
+            public double Trust;
+
+            public Uri Faceit;
+        }
+    }
+}
---
+++ Tests/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tests
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FaceitFinderAPI.FaceitFinder faceit = new FaceitFinderAPI.FaceitFinder();
+            var faceitresponce = faceit.GetResponce("Steam ID");
+            Console.WriteLine("Faceit link: " + faceitresponce.Faceit);
+            Console.WriteLine("Trust Factor: " + faceitresponce.Trust + "% from " + (100 - faceitresponce.Trust) + "%");
+        }
+    }
+}
